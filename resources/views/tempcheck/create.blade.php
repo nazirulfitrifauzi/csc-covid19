f4a68e80a410ae20285030ba82d4bacd5b8db4ec
safwan <safwan@csc.net.my>
2020-05-14 17:12:23 +0800

    add Guest and edit reporting

--- resources/views/tempcheck/create.blade.php
+++ resources/views/tempcheck/create.blade.php
@@ -56,32 +56,44 @@
 						<div class="inline-block relative w-full">
 							<select class="shadow  border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="name" required
 								onmousedown="if(this.options.length>8){this.size=8;}"  onchange='this.size=0;' onblur="this.size=0;">
-								<option>Choose Name</option>
-								<option>Hashim Bin Hassan</option>
-								<option>Mohamed Asari Bin Mohamed Isa</option>
-								<option>Azizan Bin Samad</option>
-								<option>Azura Binti Pi Ramli</option>
-								<option>Suarni Asra Binti Abdul Rohhim</option>
-								<option>Mohd Faisol bin Md Yusuf</option>
-								<option>Mohd Aizuddin Bin Mohd Yusoff</option>
-								<option>Muhammad Haqim Bin Zuhari</option>
-								<option>Nazirul Fitri Bin Fauzi</option>
-								<option>Muhamad Irfan Bin Hashim</option>
-								<option>Muhammad Anis Bin Abdul Ghani</option>
-								<option>Nur Izzah Syahira Binti Razali</option>
-								<option>Muhammad Safwan Bin Azman</option>
-								<option>Fadhilrul Hafidz Bin Saifuddin</option>
-								<option>Syed Nasir Bin Syed Abd Jalil</option>
-								<option>Md Sidek Bi Abd Hamid</option>
-								<option>Ahmad Zuwari Bin Muhammad</option>
-								<option>Alif Hakimi Lokman</option>
-								<option>Zarshaid</option>
-								<option>Wati Setiawati</option>
-								<option>Nor Hidayah Binti Ahmad Zaini</option>
-							</select>
+								 <optgroup label="Guest">
+								 	<option value="" disabled selected>Choose Name</option>
+									<option>Ryan</option>
+									<option>Faiz</option>
+									<option>Fazli</option>
+									<option>Athirah</option>
+									<option>Nashar</option>
+							    </optgroup>
+
+								<optgroup label="Staff Name">
+									<option>Hashim Bin Hassan</option>
+									<option>Mohamed Asari Bin Mohamed Isa</option>
+									<option>Azizan Bin Samad</option>
+									<option>Azura Binti Pi Ramli</option>
+									<option>Suarni Asra Binti Abdul Rohhim</option>
+									<option>Mohd Faisol bin Md Yusuf</option>
+									<option>Mohd Aizuddin Bin Mohd Yusoff</option>
+									<option>Muhammad Haqim Bin Zuhari</option>
+									<option>Nazirul Fitri Bin Fauzi</option>
+									<option>Muhamad Irfan Bin Hashim</option>
+									<option>Muhammad Anis Bin Abdul Ghani</option>
+									<option>Nur Izzah Syahira Binti Razali</option>
+									<option>Muhammad Safwan Bin Azman</option>
+									<option>Fadhilrul Hafidz Bin Saifuddin</option>
+									<option>Syed Nasir Bin Syed Abd Jalil</option>
+									<option>Md Sidek Bi Abd Hamid</option>
+									<option>Ahmad Zuwari Bin Muhammad</option>
+									<option>Alif Hakimi Lokman</option>
+									<option>Zarshaid</option>
+									<option>Wati Setiawati</option>
+									<option>Nor Hidayah Binti Ahmad Zaini</option>
+	                            </optgroup>
+						</select>
 							
 						</div>
 					</div>
+
+			
 					
 					<div class="mb-6">
 						<div class="flex">
